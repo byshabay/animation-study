bc064cec18e5e3715e971ed49a64e7e72eb2def3
Alyona Shabaykina <shabbaykina@mail.ru>
2021-10-15 17:21:31 +0300

    tryam

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
 
+    // 1.VERTICAL SCROLL TO HORIZONTAL SCROLL START
+    window.addEventListener('wheel', function (event) {
+        event.preventDefault();
+
+        window.scrollBy({
+            left: event.deltaY < 0 ? -200 : 200,
+        });
+    }, { passive: false });
+    // 1.VERTICAL SCROLL TO HORIZONTAL SCROLL END
+
 });
+
