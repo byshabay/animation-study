c8f5275e3e5a62381c499f39e9ab702ca79ba556
Alyona Shabaykina <shabbaykina@mail.ru>
2021-10-19 11:40:14 +0300

    Add horizontal smooth scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 $(document).ready(function () {
 
     // 1.VERTICAL SCROLL TO HORIZONTAL SCROLL START
-    window.addEventListener('wheel', function (event) {
-        event.preventDefault();
+    // window.addEventListener('wheel', function (event) {
+    //     event.preventDefault();
 
-        window.scrollBy({
-            left: event.deltaY < 0 ? -200 : 200,
-        });
-    }, { passive: false });
+    //     window.scrollBy({
+    //         left: event.deltaY < 0 ? -200 : 200,
+    //     });
+    // }, { passive: false });
+
+    var blocks = document.getElementsByClassName("block");
+    var container = document.getElementsByClassName("container");
+    var hs = new HorizontalScroll.default({
+        blocks: blocks,
+        container: container,
+    });
+
     // 1.VERTICAL SCROLL TO HORIZONTAL SCROLL END
 
 });
